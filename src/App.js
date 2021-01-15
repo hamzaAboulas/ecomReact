bcda43616274cda2508f9c220bddc936fe3638e8
teamProjectSerfa <60262218+teamProjectSerfa@users.noreply.github.com>
2021-01-15 20:28:15 +0100

    styled the header componenet.....not Complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './component/header/header.component';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
